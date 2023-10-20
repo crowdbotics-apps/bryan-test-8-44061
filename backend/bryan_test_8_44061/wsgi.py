df28673f31d48c14049d3d0ba2a9c5017707c291
Crowdbotics <team@crowdbotics.com>
2023-10-20 17:36:11 +0000

    Initial commit

---
+++ backend/bryan_test_8_44061/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for bryan_test_8_44061 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "bryan_test_8_44061.settings"
+)
+
+application = get_wsgi_application()
